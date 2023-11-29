af9c1b13881efa29a39d88025f32e21937959c73
Alexander Kuiper <kuiper9000@gmail.com>
2023-11-29 20:13:12 +0100

    formatting issue resolved

--- script/contact.js
+++ script/contact.js
@@ -1,119 +1,116 @@
+// function createFile() {
+//     var text = document.getElementById('textInput').value;
+//     var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
+//     var url = window.URL.createObjectURL(blob);
 
-    // function createFile() {
-    //     var text = document.getElementById('textInput').value;
-    //     var blob = new Blob([text], { type: "text/plain;charset=utf-8" });
-    //     var url = window.URL.createObjectURL(blob);
-
-    //     var a = document.createElement('a');
-    //     a.href = url;
-    //     a.download = 'downloaded_file.txt';
-    //     a.style.display = 'none';
-    //     document.body.appendChild(a);
-    //     a.click();
-    //     document.body.removeChild(a);
-    //     window.URL.revokeObjectURL(url);
-    // }
-
+//     var a = document.createElement('a');
+//     a.href = url;
+//     a.download = 'downloaded_file.txt';
+//     a.style.display = 'none';
+//     document.body.appendChild(a);
+//     a.click();
+//     document.body.removeChild(a);
+//     window.URL.revokeObjectURL(url);
+// }
 
 //initialize form and map
-createForm()
-createMap()
+createForm();
+createMap();
 //constant elements
 const popup = document.getElementById("popup");
 const span = document.getElementsByClassName("close")[0];
-const submit = document.getElementById("send")
+const submit = document.getElementById("send");
 
 //event listener for the send button
 
-submit.addEventListener("click", function(evt){
+submit.addEventListener("click", function (evt) {
   //Stop reloading page
-  evt.preventDefault()
+  evt.preventDefault();
   //get the info from the form
-  createJSONinfo()
- //message send message
- popup.style.display = "block";
-})
+  createJSONinfo();
+  //message send message
+  popup.style.display = "block";
+});
 
 // When the user clicks on x or on the window it will close the popup
-span.onclick = function() {
+span.onclick = function () {
   popup.style.display = "none";
-}
-window.onclick = function(event) {
+};
+window.onclick = function (event) {
   if (event.target == popup) {
     popup.style.display = "none";
   }
-}
-
+};
 
 //function section
 
 //A function that creates json object ready to be send somewhere else to a email or database
-function createJSONinfo(){
+function createJSONinfo() {
   //get all the values
-  const name = document.getElementById("name")
-  const email = document.getElementById("email")
-  const message = document.getElementById("message")
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
   //put it a object
   let info = {
-    name : name.value,
+    name: name.value,
     email: email.value,
-    message: message.value
-  }
+    message: message.value,
+  };
 
-    //object to json 
-    console.log(JSON.stringify(info))
-    //form reset
-    name.value =""
-    email.value=""
-    message.value=""
+  //object to json
+  console.log(JSON.stringify(info));
+  //form reset
+  name.value = "";
+  email.value = "";
+  message.value = "";
 }
 
-//Create the form 
-function createForm(){
+//Create the form
+function createForm() {
   //container
-  const formContainer = document.querySelector(".form-container")
-  const form = document.createElement("form")
-  
+  const formContainer = document.querySelector(".form-container");
+  const form = document.createElement("form");
+
   //create all the element required in the form
-  const title = document.createElement("h1")
-  title.textContent = "Contact Me"
+  const title = document.createElement("h1");
+  title.textContent = "Contact Me";
 
-  const nameInput = document.createElement("INPUT")
-  nameInput.setAttribute("type", "text")
-  nameInput.setAttribute("id", "name")
-  nameInput.setAttribute("placeholder", "Enter your name")
-  nameInput.setAttribute("autocomplete", "given-name")
+  const nameInput = document.createElement("INPUT");
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("id", "name");
+  nameInput.setAttribute("placeholder", "Enter your name");
+  nameInput.setAttribute("autocomplete", "given-name");
 
-  const emaiInput = document.createElement("INPUT")
+  const emaiInput = document.createElement("INPUT");
 
-  emaiInput.setAttribute("type", "email")
-  emaiInput.setAttribute("id", "email")
-  emaiInput.setAttribute("placeholder", "Enter a valid email adress")
-  emaiInput.setAttribute("autocomplete", "email")
+  emaiInput.setAttribute("type", "email");
+  emaiInput.setAttribute("id", "email");
+  emaiInput.setAttribute("placeholder", "Enter a valid email adress");
+  emaiInput.setAttribute("autocomplete", "email");
 
-  const messageInput = document.createElement("textarea")
+  const messageInput = document.createElement("textarea");
 
-  messageInput.setAttribute("id", "message")
-  messageInput.setAttribute("placeholder", "Message")
+  messageInput.setAttribute("id", "message");
+  messageInput.setAttribute("placeholder", "Message");
 
-  const btn = document.createElement("button")
-  btn.setAttribute("id", "send")
-  btn.textContent = "Send"
-  
+  const btn = document.createElement("button");
+  btn.setAttribute("id", "send");
+  btn.textContent = "Send";
+
   //append all elements and add them to the form an container
-  form.appendChild(title)
-  form.appendChild(nameInput)
-  form.appendChild(emaiInput)
-  form.appendChild(messageInput)
-  form.append(btn)
-  formContainer.appendChild(form)
+  form.appendChild(title);
+  form.appendChild(nameInput);
+  form.appendChild(emaiInput);
+  form.appendChild(messageInput);
+  form.append(btn);
+  formContainer.appendChild(form);
 }
 
-function createMap(){
-  let map = L.map('map').setView([58.38908347907847, 15.565484313558347], 15)
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+function createMap() {
+  let map = L.map("map").setView([58.38908347907847, 15.565484313558347], 15);
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 }
--- script/main.js
+++ script/main.js
@@ -1,148 +1,114 @@
 //The function fetch the json info for the left side of the page
-async function getObjectTest(){
+async function getObjectTest() {
   //fetch and check
-  const respons = await fetch("./data/personalInfo.json")
-  if (respons.ok){
-    const data = await respons.json()
+  const respons = await fetch("./data/personalInfo.json");
+  if (respons.ok) {
+    const data = await respons.json();
     //go through the data and sort object keys and content
     for (const element in data) {
       //create new elements
-      const newUl = document.createElement("ul")
-      const newTitle = document.createElement("h4")
-      const container = document.querySelector(".personal-summary")
+      const newUl = document.createElement("ul");
+      const newTitle = document.createElement("h4");
+      const container = document.querySelector(".personal-summary");
       //keys are turned into titles
-      newTitle.textContent = element
-      container.appendChild(newTitle)
+      newTitle.textContent = element;
+      container.appendChild(newTitle);
       //class assigned
-      newUl.className= "personal-info-items"
+      newUl.className = "personal-info-items";
       //info will contain all attributes of the key / title
-      let info = data[element]
-      info.forEach(infoBlock => {
-        let newLi = document.createElement("li")
+      let info = data[element];
+      info.forEach((infoBlock) => {
+        let newLi = document.createElement("li");
         //if the attribute is just a string then it will be added as a new LI element
-        if(typeof infoBlock == "string"){
-        
-        newLi.textContent= infoBlock
-        
-        
-    }else{
-    //if it is an array a function to create sublists is called
-     createSubList(infoBlock,newUl)
-      
-    }
-    newUl.appendChild(newLi)
-  })
-      container.appendChild(newUl)
-
-
-      
+        if (typeof infoBlock == "string") {
+          newLi.textContent = infoBlock;
+        } else {
+          //if it is an array a function to create sublists is called
+          createSubList(infoBlock, newUl);
+        }
+        newUl.appendChild(newLi);
+      });
+      container.appendChild(newUl);
     }
   }
-
 }
 
-
-function createSubList(subList,upperUl){
-
-  
-  subList.forEach(element => {
-    const newLi = document.createElement("li")
-    if(typeof element == "string" && element.length != 0){
-        newLi.textContent= element
-        upperUl.appendChild(newLi)
-        
-
-    }else{
-      const ul = document.createElement("ul")
-      ul.className = "personal-info-items"
-      newLi.appendChild(ul)
-      upperUl.appendChild(newLi)
-      createSubList(element,ul)
+function createSubList(subList, upperUl) {
+  subList.forEach((element) => {
+    const newLi = document.createElement("li");
+    if (typeof element == "string" && element.length != 0) {
+      newLi.textContent = element;
+      upperUl.appendChild(newLi);
+    } else {
+      const ul = document.createElement("ul");
+      ul.className = "personal-info-items";
+      newLi.appendChild(ul);
+      upperUl.appendChild(newLi);
+      createSubList(element, ul);
     }
-  }
-  )
-}
-
-async function createMain(){
-  const mainElement = document.querySelector("main")
-  const respons = await fetch("./data/cv.json")
-  if (respons.ok){
-    const mainData = await respons.json()
-    
-    const mainAtt =Object.keys(mainData)
-    const newH1 = document.createElement("h1")
-    
-    newH1.textContent= mainAtt[0]
-    mainElement.appendChild(newH1)
-    
-  
-
-    for (const element in mainData[mainAtt]){
-      const newH4title = document.createElement("h4")
-      newH4title.textContent = element
-      mainElement.appendChild(newH4title)
-      let something = mainData[mainAtt]
-      if(typeof something[element] == "string"){
-      const newP = document.createElement("p")
-      newP.className = "description"
-      newP.textContent = something[element];
-      mainElement.appendChild(newP)
-
-    }else{
-      
-      const newUL = document.createElement("Ul")
-      newUL.className = "sublist"
-
-      createCvSublist(something[element], newUL)
-      mainElement.appendChild(newUL)
-
-    }
-    
-  }
-
-
-}
-}
-
-
-function createCvSublist(sublist, parentUl){
-  
-
-  const keys = Object.keys(sublist)
-  
-  keys.forEach(element => {
-    const newBoldLi = document.createElement("li")
-    console.log(element.length + " - " +element)
-    if( element.length > 1){
-    newBoldLi.textContent = element 
-    newBoldLi.className = "bold-sublist"
-    parentUl.appendChild(newBoldLi)
-    }
-
-    if(typeof sublist[element]== "string"){
-      const newLi = document.createElement("li")
-      newLi.textContent= sublist[element]
-      parentUl.appendChild(newLi)
-
-    }else{
-      const newLi = document.createElement("li")
-      const subUL = document.createElement("ul")
-      
-      subUL.className += "sub-" + parentUl.className 
-      createCvSublist(sublist[element], subUL)
-      parentUl.appendChild(newLi)
-      newLi.appendChild(subUL)
-    }
-    
   });
-    
-  
-
-
-
-
 }
 
+async function createMain() {
+  const mainElement = document.querySelector("main");
+  const respons = await fetch("./data/cv.json");
+  if (respons.ok) {
+    const mainData = await respons.json();
 
-createMain()
-getObjectTest()
+    const mainAtt = Object.keys(mainData);
+    const newH1 = document.createElement("h1");
+
+    newH1.textContent = mainAtt[0];
+    mainElement.appendChild(newH1);
+
+    for (const element in mainData[mainAtt]) {
+      const newH4title = document.createElement("h4");
+      newH4title.textContent = element;
+      mainElement.appendChild(newH4title);
+      let something = mainData[mainAtt];
+      if (typeof something[element] == "string") {
+        const newP = document.createElement("p");
+        newP.className = "description";
+        newP.textContent = something[element];
+        mainElement.appendChild(newP);
+      } else {
+        const newUL = document.createElement("Ul");
+        newUL.className = "sublist";
+
+        createCvSublist(something[element], newUL);
+        mainElement.appendChild(newUL);
+      }
+    }
+  }
+}
+
+function createCvSublist(sublist, parentUl) {
+  const keys = Object.keys(sublist);
+
+  keys.forEach((element) => {
+    const newBoldLi = document.createElement("li");
+    console.log(element.length + " - " + element);
+    if (element.length > 1) {
+      newBoldLi.textContent = element;
+      newBoldLi.className = "bold-sublist";
+      parentUl.appendChild(newBoldLi);
+    }
+
+    if (typeof sublist[element] == "string") {
+      const newLi = document.createElement("li");
+      newLi.textContent = sublist[element];
+      parentUl.appendChild(newLi);
+    } else {
+      const newLi = document.createElement("li");
+      const subUL = document.createElement("ul");
+
+      subUL.className += "sub-" + parentUl.className;
+      createCvSublist(sublist[element], subUL);
+      parentUl.appendChild(newLi);
+      newLi.appendChild(subUL);
+    }
+  });
+}
+
+createMain();
+getObjectTest();
